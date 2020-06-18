b139ef32522afed23358ce03661b47e3cd0b6a4f
FlowerOfSorrow <58352696+FlowerOfSorrow@users.noreply.github.com>
2020-06-18 17:00:43 +0900

    Add files via upload

--- compare.cpp
+++ compare.cpp
@@ -1,16 +1,7 @@
 #include "bits/stdc++.h"
-#include "ext/pb_ds/assoc_container.hpp"
-#include "ext/pb_ds/tree_policy.hpp"
-#include "ext/rope"
 using namespace std;
 using namespace chrono;
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
 mt19937 rng(high_resolution_clock::now().time_since_epoch().count());
-mt19937_64 rngll(high_resolution_clock::now().time_since_epoch().count());
-template<typename T> T ctmax(T &x, const T &y){ return x = max(x, y); }
-template<typename T> T ctmin(T &x, const T &y){ return x = min(x, y); }
-template<typename T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 
 
